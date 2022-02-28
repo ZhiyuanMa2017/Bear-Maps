5d517899bf02c5d97d131146bda87863ae574159
Zhiyuan Ma <631253076@qq.com>
2022-02-27 20:46:05 -0500

    update bear maps

---
+++ src/main/java/GraphDB.java
@@ -0,0 +1,310 @@
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+
+/**
+ * Graph for storing all of the intersection (vertex) and road (edge) information.
+ * Uses your GraphBuildingHandler to convert the XML files into a graph. Your
+ * code must include the vertices, adjacent, distance, closest, lat, and lon
+ * methods. You'll also need to include instance variables and methods for
+ * modifying the graph (e.g. addNode and addEdge).
+ *
+ * @author Alan Yao, Josh Hug
+ */
+public class GraphDB {
+    /**
+     * Your instance variables for storing the graph. You should consider
+     * creating helper classes, e.g. Node, Edge, etc.
+     */
+    Map<Long, Node> vertices = new LinkedHashMap<>();
+    private final Map<Long, Edge> edges = new LinkedHashMap<>();
+    private final Map<String, Long> path = new LinkedHashMap<>();
+    private Trie trie = new Trie();
+
+    static class Node {
+        long id;
+        double lat;
+        double lon;
+        List<Long> adjNode = new ArrayList<>();
+        String name;
+
+        Node(long id, double lat, double lon) {
+            this.id = id;
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+
+    }
+
+    static class Edge {
+        long id;
+        List<Long> vertexList;
+        String name;
+
+
+        Edge(long id, List<Long> vertexList) {
+            this.id = id;
+            this.vertexList = vertexList;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    /**
+     * Example constructor shows how to create and start an XML parser.
+     * You do not need to modify this constructor, but you're welcome to do so.
+     *
+     * @param dbPath Path to the XML file to be parsed.
+     */
+    public GraphDB(String dbPath) {
+        try {
+//            File inputFile = new File(dbPath);
+//            FileInputStream inputStream = new FileInputStream(inputFile);
+            // GZIPInputStream stream = new GZIPInputStream(inputStream);
+
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            GraphBuildingHandler gbh = new GraphBuildingHandler(this);
+//            saxParser.parse(inputStream, gbh);
+            saxParser.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream(dbPath), gbh);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        clean();
+    }
+
+    /**
+     * Helper to process strings into their "cleaned" form, ignoring punctuation and capitalization.
+     *
+     * @param s Input string.
+     * @return Cleaned string.
+     */
+    static String cleanString(String s) {
+        return s.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+    }
+
+    /**
+     * Remove nodes with no connections from the graph.
+     * While this does not guarantee that any two nodes in the remaining graph are connected,
+     * we can reasonably assume this since typically roads are connected.
+     */
+    private void clean() {
+        Map<Long, Node> aftervertices = new LinkedHashMap<>();
+        for (Node node : vertices.values()) {
+            if (node.adjNode.size() != 0) {
+                aftervertices.put(node.id, node);
+            }
+        }
+        vertices = aftervertices;
+    }
+
+    /**
+     * Returns an iterable of all vertex IDs in the graph.
+     *
+     * @return An iterable of id's of all vertices in the graph.
+     */
+    Iterable<Long> vertices() {
+        return vertices.keySet();
+    }
+
+    /**
+     * Returns ids of all vertices adjacent to v.
+     *
+     * @param v The id of the vertex we are looking adjacent to.
+     * @return An iterable of the ids of the neighbors of v.
+     */
+    Iterable<Long> adjacent(long v) {
+        List<Long> adjnodes = new ArrayList<>();
+        Node thisnode = vertices.get(v);
+        for (Long aLong : thisnode.adjNode) {
+            adjnodes.add(aLong);
+        }
+        return adjnodes;
+    }
+
+    /**
+     * Returns the great-circle distance between vertices v and w in miles.
+     * Assumes the lon/lat methods are implemented properly.
+     * <a href="https://www.movable-type.co.uk/scripts/latlong.html">Source</a>.
+     *
+     * @param v The id of the first vertex.
+     * @param w The id of the second vertex.
+     * @return The great-circle distance between the two locations from the graph.
+     */
+    double distance(long v, long w) {
+        return distance(lon(v), lat(v), lon(w), lat(w));
+    }
+
+    static double distance(double lonV, double latV, double lonW, double latW) {
+        double phi1 = Math.toRadians(latV);
+        double phi2 = Math.toRadians(latW);
+        double dphi = Math.toRadians(latW - latV);
+        double dlambda = Math.toRadians(lonW - lonV);
+
+        double a = Math.sin(dphi / 2.0) * Math.sin(dphi / 2.0);
+        a += Math.cos(phi1) * Math.cos(phi2) * Math.sin(dlambda / 2.0) * Math.sin(dlambda / 2.0);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return 3963 * c;
+    }
+
+    /**
+     * Returns the initial bearing (angle) between vertices v and w in degrees.
+     * The initial bearing is the angle that, if followed in a straight line
+     * along a great-circle arc from the starting point, would take you to the
+     * end point.
+     * Assumes the lon/lat methods are implemented properly.
+     * <a href="https://www.movable-type.co.uk/scripts/latlong.html">Source</a>.
+     *
+     * @param v The id of the first vertex.
+     * @param w The id of the second vertex.
+     * @return The initial bearing between the vertices.
+     */
+    double bearing(long v, long w) {
+        return bearing(lon(v), lat(v), lon(w), lat(w));
+    }
+
+    static double bearing(double lonV, double latV, double lonW, double latW) {
+        double phi1 = Math.toRadians(latV);
+        double phi2 = Math.toRadians(latW);
+        double lambda1 = Math.toRadians(lonV);
+        double lambda2 = Math.toRadians(lonW);
+
+        double y = Math.sin(lambda2 - lambda1) * Math.cos(phi2);
+        double x = Math.cos(phi1) * Math.sin(phi2);
+        x -= Math.sin(phi1) * Math.cos(phi2) * Math.cos(lambda2 - lambda1);
+        return Math.toDegrees(Math.atan2(y, x));
+    }
+
+    /**
+     * Returns the vertex closest to the given longitude and latitude.
+     *
+     * @param lon The target longitude.
+     * @param lat The target latitude.
+     * @return The id of the node in the graph closest to the target.
+     */
+    long closest(double lon, double lat) {
+        double mindis = Double.MAX_VALUE;
+        double curdis;
+        long minid = 0;
+        for (Long id : vertices.keySet()) {
+            curdis = distance(lon, lat, vertices.get(id).lon, vertices.get(id).lat);
+            if (curdis < mindis) {
+                mindis = curdis;
+                minid = id;
+            }
+        }
+        return minid;
+    }
+
+    /**
+     * Gets the longitude of a vertex.
+     *
+     * @param v The id of the vertex.
+     * @return The longitude of the vertex.
+     */
+    double lon(long v) {
+        if (vertices.containsKey(v)) {
+            return vertices.get(v).lon;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Gets the latitude of a vertex.
+     *
+     * @param v The id of the vertex.
+     * @return The latitude of the vertex.
+     */
+    double lat(long v) {
+        if (vertices.containsKey(v)) {
+            return vertices.get(v).lat;
+        } else {
+            return 0;
+        }
+    }
+
+    void addNode(long id, double lat, double lon) {
+        vertices.put(id, new Node(id, lat, lon));
+    }
+
+
+    void addEdge(long id, List<Long> vertexList) {
+        edges.put(id, new Edge(id, vertexList));
+        for (int i = 0; i < vertexList.size() - 1; i++) {
+            long firstid = vertexList.get(i);
+            long secondid = vertexList.get(i + 1);
+            Node firstnode = vertices.get(firstid);
+            Node secondnode = vertices.get(secondid);
+            firstnode.adjNode.add(secondid);
+            secondnode.adjNode.add(firstid);
+            path.put(firstid + "to" + secondid, id);
+            path.put(secondid + "to" + firstid, id);
+        }
+    }
+
+    void setNodename(long id, String name) {
+        vertices.get(id).setName(name);
+        trie.put(id, name, vertices.get(id).lat, vertices.get(id).lon);
+
+    }
+
+    void setWayname(long id, String name) {
+        edges.get(id).setName(name);
+    }
+
+    Long findWayid(Long v, Long w) {
+        String name1 = v + "to" + w;
+        if (path.containsKey(name1)) {
+            return path.get(name1);
+        } else {
+            return (long) -1;
+        }
+    }
+
+    String getWayname(Long id) {
+        if (edges.containsKey(id)) {
+            if (edges.get(id).name != null) {
+                return edges.get(id).name;
+            }
+        }
+        return Router.NavigationDirection.UNKNOWN_ROAD;
+    }
+
+    List<String> getLocationsByPrefix(String prefix) {
+        return trie.keysWithPrefix(prefix);
+    }
+
+    List<Map<String, Object>> getLocations(String name) {
+        List<Map<String, Object>> res = new ArrayList<>();
+        String key = cleanString(name);
+        if (trie.contains(key)) {
+            res = trie.getnodeinfo(key);
+        }
+        return res;
+    }
+
+
+}
---
+++ src/main/java/Router.java
@@ -0,0 +1,280 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class provides a shortestPath method for finding routes between two points
+ * on the map. Start by using Dijkstra's, and if your code isn't fast enough for your
+ * satisfaction (or the autograder), upgrade your implementation by switching it to A*.
+ * Your code will probably not be fast enough to pass the autograder unless you use A*.
+ * The difference between A* and Dijkstra's is only a couple of lines of code, and boils
+ * down to the priority you use to order your vertices.
+ */
+public class Router {
+    /**
+     * Return a List of longs representing the shortest path from the node
+     * closest to a start location and the node closest to the destination
+     * location.
+     *
+     * @param g       The graph to use.
+     * @param stlon   The longitude of the start location.
+     * @param stlat   The latitude of the start location.
+     * @param destlon The longitude of the destination location.
+     * @param destlat The latitude of the destination location.
+     * @return A list of node id's in the order visited on the shortest path.
+     */
+    public static List<Long> shortestPath(GraphDB g, double stlon, double stlat,
+                                          double destlon, double destlat) {
+        long startid = g.closest(stlon, stlat);
+        long goalid = g.closest(destlon, destlat);
+
+        Solver solver = new Solver(g, g.vertices.get(startid), g.vertices.get(goalid));
+
+        Stack<Long> sol = solver.solution();
+        if (sol == null || sol.isEmpty()) {
+            return new LinkedList<>();
+        }
+
+        List<Long> sp = new LinkedList<>(sol);
+        Collections.reverse(sp);
+        return sp;
+    }
+
+    /**
+     * Create the list of directions corresponding to a route on the graph.
+     *
+     * @param g     The graph to use.
+     * @param route The route to translate into directions. Each element
+     *              corresponds to a node from the graph in the route.
+     * @return A list of NavigatiionDirection objects corresponding to the input
+     * route.
+     */
+    public static List<NavigationDirection> routeDirections(GraphDB g, List<Long> route) {
+        if (route.size() < 2) {
+            return null;
+        }
+
+        List<NavigationDirection> nd = new ArrayList<>();
+
+        double curbearing = 0.0;
+        int direction = NavigationDirection.START;
+        long prevwayid = g.findWayid(route.get(0), route.get(1));
+        double dis = 0.0;
+
+        for (int i = 1; i < route.size(); i++) {
+            long previd = route.get(i - 1);
+            long curid = route.get(i);
+            double prevbearing = curbearing;
+            curbearing = g.bearing(previd, curid);
+
+            long curwayid = g.findWayid(previd, curid);
+            String curwayname = g.getWayname(curwayid);
+            String prevwayname = g.getWayname(prevwayid);
+            if (curwayname.equals(prevwayname)) {
+                dis += g.distance(previd, curid);
+                continue;
+            }
+
+            NavigationDirection navigationDirection = new NavigationDirection();
+            navigationDirection.direction = direction;
+            navigationDirection.distance = dis;
+            navigationDirection.way = g.getWayname(prevwayid);
+
+            nd.add(navigationDirection);
+
+            direction = getDirection(prevbearing, curbearing);
+            dis = g.distance(previd, curid);
+            prevwayid = curwayid;
+        }
+
+        NavigationDirection navigationDirection = new NavigationDirection();
+        navigationDirection.direction = direction;
+        navigationDirection.distance = dis;
+        navigationDirection.way = g.getWayname(prevwayid);
+
+        nd.add(navigationDirection);
+
+        return nd;
+    }
+
+    private static int getDirection(double p, double c) {
+        if (p < 0) {
+            p += 360;
+        }
+        if (c < 0) {
+            c += 360;
+        }
+
+        double diff = c - p;
+
+        if (diff < -180) {
+            diff += 360;
+        }
+        if (diff > 180) {
+            diff -= 360;
+        }
+
+        if (diff > -15.0 && diff < 15.0) {
+            return NavigationDirection.STRAIGHT;
+        } else if (diff > -30.0 && diff < 30.0) {
+            if (diff > 0) {
+                return NavigationDirection.SLIGHT_RIGHT;
+            } else {
+                return NavigationDirection.SLIGHT_LEFT;
+            }
+        } else if (diff > -100.0 && diff < 100.0) {
+            if (diff > 0) {
+                return NavigationDirection.RIGHT;
+            } else {
+                return NavigationDirection.LEFT;
+            }
+        } else {
+            if (diff > 0) {
+                return NavigationDirection.SHARP_RIGHT;
+            } else {
+                return NavigationDirection.SHARP_LEFT;
+            }
+        }
+    }
+
+
+    /**
+     * Class to represent a navigation direction, which consists of 3 attributes:
+     * a direction to go, a way, and the distance to travel for.
+     */
+    public static class NavigationDirection {
+
+        /**
+         * Integer constants representing directions.
+         */
+        public static final int START = 0;
+        public static final int STRAIGHT = 1;
+        public static final int SLIGHT_LEFT = 2;
+        public static final int SLIGHT_RIGHT = 3;
+        public static final int RIGHT = 4;
+        public static final int LEFT = 5;
+        public static final int SHARP_LEFT = 6;
+        public static final int SHARP_RIGHT = 7;
+
+        /**
+         * Number of directions supported.
+         */
+        public static final int NUM_DIRECTIONS = 8;
+
+        /**
+         * A mapping of integer values to directions.
+         */
+        public static final String[] DIRECTIONS = new String[NUM_DIRECTIONS];
+
+        /**
+         * Default name for an unknown way.
+         */
+        public static final String UNKNOWN_ROAD = "unknown road";
+
+        /** Static initializer. */
+        static {
+            DIRECTIONS[START] = "Start";
+            DIRECTIONS[STRAIGHT] = "Go straight";
+            DIRECTIONS[SLIGHT_LEFT] = "Slight left";
+            DIRECTIONS[SLIGHT_RIGHT] = "Slight right";
+            DIRECTIONS[LEFT] = "Turn left";
+            DIRECTIONS[RIGHT] = "Turn right";
+            DIRECTIONS[SHARP_LEFT] = "Sharp left";
+            DIRECTIONS[SHARP_RIGHT] = "Sharp right";
+        }
+
+        /**
+         * The direction a given NavigationDirection represents.
+         */
+        int direction;
+        /**
+         * The name of the way I represent.
+         */
+        String way;
+        /**
+         * The distance along this way I represent.
+         */
+        double distance;
+
+        /**
+         * Create a default, anonymous NavigationDirection.
+         */
+        public NavigationDirection() {
+            this.direction = STRAIGHT;
+            this.way = UNKNOWN_ROAD;
+            this.distance = 0.0;
+        }
+
+        public String toString() {
+            return String.format("%s on %s and continue for %.3f miles.",
+                    DIRECTIONS[direction], way, distance);
+        }
+
+        /**
+         * Takes the string representation of a navigation direction and converts it into
+         * a Navigation Direction object.
+         *
+         * @param dirAsString The string representation of the NavigationDirection.
+         * @return A NavigationDirection object representing the input string.
+         */
+        public static NavigationDirection fromString(String dirAsString) {
+            String regex = "([a-zA-Z\\s]+) on ([\\w\\s]*) and continue for ([0-9\\.]+) miles\\.";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(dirAsString);
+            NavigationDirection nd = new NavigationDirection();
+            if (m.matches()) {
+                String direction = m.group(1);
+                if (direction.equals("Start")) {
+                    nd.direction = NavigationDirection.START;
+                } else if (direction.equals("Go straight")) {
+                    nd.direction = NavigationDirection.STRAIGHT;
+                } else if (direction.equals("Slight left")) {
+                    nd.direction = NavigationDirection.SLIGHT_LEFT;
+                } else if (direction.equals("Slight right")) {
+                    nd.direction = NavigationDirection.SLIGHT_RIGHT;
+                } else if (direction.equals("Turn right")) {
+                    nd.direction = NavigationDirection.RIGHT;
+                } else if (direction.equals("Turn left")) {
+                    nd.direction = NavigationDirection.LEFT;
+                } else if (direction.equals("Sharp left")) {
+                    nd.direction = NavigationDirection.SHARP_LEFT;
+                } else if (direction.equals("Sharp right")) {
+                    nd.direction = NavigationDirection.SHARP_RIGHT;
+                } else {
+                    return null;
+                }
+
+                nd.way = m.group(2);
+                try {
+                    nd.distance = Double.parseDouble(m.group(3));
+                } catch (NumberFormatException e) {
+                    return null;
+                }
+                return nd;
+            } else {
+                // not a valid nd
+                return null;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof NavigationDirection) {
+                return direction == ((NavigationDirection) o).direction
+                        && way.equals(((NavigationDirection) o).way)
+                        && distance == ((NavigationDirection) o).distance;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(direction, way, distance);
+        }
+    }
+}
